323a0a4f119202b062dd6aa3ea522a58c168f1ce
yusnulmagfira <yusnulmagfira@gmail.com>
2025-09-08 17:55:40 +0800

    Add files via upload

---
+++ Day1.java
@@ -0,0 +1,7 @@
+package YusnulMagfira; //package
+
+public class Day1 { //class
+    public static void main(String[] args) { //class main
+        System.out.println("Hello World"); //tampilan untuk mencetak hello world
+    }
+}
