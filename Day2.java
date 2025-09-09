0e8ed83668b376184fa7bc8522650d3c20ff9c60
yusnulmagfira <yusnulmagfira@gmail.com>
2025-09-09 16:47:38 +0800

    Day2.java

---
+++ Day2.java
@@ -0,0 +1,10 @@
+package YusnulMagfira;
+
+public class Day2 {
+    public static void main(String[] args) {
+        System.out.print("Hello World"); //print berjejer ke samping
+        System.out.println("Hello World"); //mencetak baris baru ke bawah
+        String teks = "Hello World";
+        System.out.printf("%s",teks); //printf untuk mencetak format, '%s' untuk menampilkan string(teks)
+    }
+}
