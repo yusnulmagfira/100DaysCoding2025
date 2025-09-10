2925aad74efc8e79b36090f970fddefe933a6ff2
yusnulmagfira <yusnulmagfira@gmail.com>
2025-09-10 20:26:23 +0800

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,13 @@
+package YusnulMagfira;
+
+public class Day3 {
+    public static void main(String[] args) {
+        int angka = 1000; // tanda garis miring 2 untuk menambahkan kometar 1 baris
+        int angka2 = 100;
+        System.out.println(angka*angka2);
+        /* tanda garis miring 1 dan bintang 1
+        untuk menambahkan komentar multi baris yang lebih dari 1 baris
+         */
+
+    }
+}
