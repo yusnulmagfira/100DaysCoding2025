4b94d2f64de73c604a40fb0d005f2dc4eb2a2b47
yusnulmagfira <yusnulmagfira@gmail.com>
2025-09-11 20:06:12 +0800

    Day4.java

---
+++ Day4.java
@@ -0,0 +1,9 @@
+public class Day4 {
+    public static void main(String[] args) {
+        System.out.print("Halo Nama \n Saya"); // (\n)new line (pindah ke baris baru)
+        System.out.print("Yusnul \t magfira"); //(\t) tab (spasi horizontal yang terformat)
+        System.out.println("Saya Tinggal Di\bTapango");//(\b) backspace (menghapus karakter sebelumnya pada output)
+        System.out.println("Saya\r Jurusan Informatika"); //(\r) return (kembali ke awal baris)
+
+    }
+}
