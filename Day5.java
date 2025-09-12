1dccc3006ac9228d9a90f4e574ec1d778099d4fd
yusnulmagfira <yusnulmagfira@gmail.com>
2025-09-12 21:22:43 +0800

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,17 @@
+package YusnulMagfira;
+
+public class Day5 {
+    public static void main(String[] args) {
+        byte umur = 20; //rentang type data byte -128 hingga 127
+        short hari = 327; //rentang type data short -32.768 hingga 32.767
+        int ukt =2000000; //rentang type data int -2.147.483.648 hingga 2.147.483.647
+        long nik = 7604075002010000L;//rentang type data long -9.223.372.036.854.775.808 hingga 9.223.372.036.854.775.807
+
+        System.out.println("Tahun ini saya berumur: " + umur);
+        System.out.println("Sekarang adalah hari ke : " + hari);
+        System.out.println("Biaya uang kuliah tunggal saya persemester : " + ukt);
+        System.out.println("Nomor induk saya : " + nik);
+
+
+    }
+}
