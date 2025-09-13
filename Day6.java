f585e083ce9c790a0f84837515fe7fdabcdf849d
yusnulmagfira <yusnulmagfira@gmail.com>
2025-09-13 18:55:41 +0800

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,11 @@
+package YusnulMagfira;
+
+public class Day6 {
+    public static void main(String[] args) {
+        double gaji = 2.5600000; //type data double digunakan untuk angka desimal
+        float harga = 2.500f; // pakai 'f' agar dikenali sebagai float
+
+        System.out.println("Saya bekerja dengan gaji : " + gaji  + " perbulan");
+        System.out.println("Saya membeli telur dengan harga : "+ harga +" /biji" );
+    }
+}
