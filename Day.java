085f83cb132cfb2437b38c610501ccbd5f9a4a5b
yusnulmagfira <yusnulmagfira@gmail.com>
2025-09-14 19:02:00 +0800

    Create Day.java

---
+++ Day.java
@@ -0,0 +1,12 @@
+package YusnulMagfira;
+
+public class Day7 {
+    public static void main(String[] args) {
+        char inisial = 'Y'; //tipe data char hanya memuat 1 karakter saja dan tanda petik satu
+        boolean jawaban = true; //tipe data boolean hanya memuat true dan false saja
+
+        System.out.println(" char : " + inisial);
+        System.out.println("boolean : " + jawaban);
+        
+    }
+}
