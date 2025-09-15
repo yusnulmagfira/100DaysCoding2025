28b53a960a929897b8650d6e006042d393a4ae0b
yusnulmagfira <yusnulmagfira@gmail.com>
2025-09-15 13:59:21 +0800

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,10 @@
+package YusnulMagfira;
+
+public class Day8 {
+    public static void main(String[] args) {
+        String nama = "Yusnul Magfira"; /*type data string ada digunakan untuk menyimpan teks (kumpulan karakter),
+         type data string menggunakan ("..") untuk menyimpan teks didalamnya.*/
+        System.out.println("Nama : " +nama);
+
+    }
+}
