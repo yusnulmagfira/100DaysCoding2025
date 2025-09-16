9b9d183667c3a9def25aa33f9eafdc19ce6bfc23
yusnulmagfira <yusnulmagfira@gmail.com>
2025-09-16 21:06:22 +0800

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,11 @@
+package YusnulMagfira;
+
+public class Day9 {
+    public static void main(String[] args) {
+        int umur = 20; // deklarasi dan inisialisasi variabel
+        System.out.println("Umur saya tahun ini : " + umur);
+
+        umur = 21; //update variabel
+        System.out.println("Umur saya tahun depan : " + umur);
+    }
+}
