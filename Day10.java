3c03e4f64440499b0352dbb3f76c0eb56ec162d8
yusnulmagfira <yusnulmagfira@gmail.com>
2025-09-17 21:15:16 +0800

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,9 @@
+package YusnulMagfira;
+
+public class Day10 {
+    public static void main(String[] args) {
+        final int hari;
+        hari =365; //nilai dalam suatu variabel tetap tidak bisa di update.
+        System.out.println(hari);
+    }
+}
