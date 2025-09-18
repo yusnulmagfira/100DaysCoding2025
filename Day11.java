9e000decc7178a333e5de33664c6141f3637f8ab
yusnulmagfira <yusnulmagfira@gmail.com>
2025-09-18 19:10:02 +0800

    Create Day11.java

---
+++ Day11.java
@@ -0,0 +1,14 @@
+package YusnulMagfira;
+
+import java.util.Scanner;
+
+public class Day11 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.print("masukkan angka: ");
+        int angka = sc.nextInt();
+
+
+    }
+
+}
