fcd81ee94facf4bed6d8df0208f28058da98efde
yusnulmagfira <yusnulmagfira@gmail.com>
2025-09-19 11:38:33 +0800

    Create Day12.java

---
+++ Day12.java
@@ -0,0 +1,24 @@
+package YusnulMagfira;
+
+public class Day12 {
+    public static void main(String[] args) {
+        String nama = "Yusnul Magfira";
+        String nim = "D0224021";
+        String alamat = "Tapango";
+        String hoby = "Traveling";
+        String prodi = "Teknik Informatika";
+        double ipk = 3.33;
+        int sks = 23;
+
+        System.out.println("Biodata saya: ");
+        System.out.println("nama saya : " + nama);
+        System.out.println("dengan nim : " + nim);
+        System.out.println("saya berasal dari : " + alamat);
+        System.out.println("hobby saya : " + hoby);
+        System.out.println("sekarang saya kuliah di jurusan : " + prodi);
+        System.out.println("ipk saya : " + ipk);
+        System.out.println("semester ini saya mengambil sks sebanyak : " + sks);
+
+
+    }
+}
