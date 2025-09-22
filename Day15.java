f2c839f149e3865920b986e80e9541b1706a4a28
yusnulmagfira <yusnulmagfira@gmail.com>
2025-09-22 19:10:23 +0800

    Create Day15.java

---
+++ Day15.java
@@ -0,0 +1,10 @@
+package YusnulMagfira;
+
+public class Day15 {
+    public static void main(String[] args) {
+        int angka1 = 67;
+        int angka2 = 55;
+        System.out.println("Hasil dari penjumlahan adalah : " + (angka1+angka2));
+        System.out.println("Hasil dari pengurangan adalah : " + (angka1-angka2));
+    }
+}
