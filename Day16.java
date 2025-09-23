9996a913dbc68062e188ecccb4fc2b9f6a527e96
yusnulmagfira <yusnulmagfira@gmail.com>
2025-09-23 20:22:38 +0800

    Create Day16.java

---
+++ Day16.java
@@ -0,0 +1,11 @@
+package YusnulMagfira;
+
+public class Day16 {
+    public static void main(String[] args) {
+        int kali = 10;
+        int bagi = 5;
+
+        System.out.println("hasil  dari perkalian adalah: " + (kali*bagi)); //operasi perkalian
+        System.out.println("hasil dari pembagian adalah : " + (kali/bagi));// operasi pembagian
+    }
+}
