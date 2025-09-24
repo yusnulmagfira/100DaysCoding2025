7a2fa671b4dd852912a00d156b3a4126b0242c0b
yusnulmagfira <yusnulmagfira@gmail.com>
2025-09-24 20:18:47 +0800

    Create Day17.java

---
+++ Day17.java
@@ -0,0 +1,10 @@
+package YusnulMagfira;
+
+public class Day17 {
+    public static void main(String[] args) {
+        int angka = 55;
+        int angka2 = 3;
+
+        System.out.println("hasil sisa bagi adalah : " + (angka% angka2));
+    }
+}
