0b2180a4b9f93264f182b89e7d0594acbf6ca65e
yusnulmagfira <yusnulmagfira@gmail.com>
2025-09-26 20:48:12 +0800

    Create Day19.java

---
+++ Day19.java
@@ -0,0 +1,16 @@
+package YusnulMagfira;
+
+public class Day19 {
+    public static void main(String[] args) {
+
+        double angkaDouble = 550.45;
+        byte hasilByte = (byte) angkaDouble;
+
+        System.out.println("double : "  + angkaDouble); //hasilnya 550.45
+        System.out.println("byte : " + hasilByte); //hasilnya 38 karena terjadi overflow
+
+            }
+        }
+
+
+
