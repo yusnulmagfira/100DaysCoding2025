3b37ea4c66ae3ebea35b9d3afe8c6931c5ae7658
yusnulmagfira <yusnulmagfira@gmail.com>
2025-09-29 21:41:37 +0800

    Create Day22.java

---
+++ Day22.java
@@ -0,0 +1,11 @@
+package YusnulMagfira;
+
+public class Day22 {
+    public static void main(String[] args) {
+        int sisi =3;
+        int luas = sisi*sisi;
+
+        System.out.println("sisi : " + sisi);
+        System.out.println("luas : " +luas);
+    }
+}
