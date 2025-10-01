0c0bb924b85a56afd3865bc52f962778993d17ef
yusnulmagfira <yusnulmagfira@gmail.com>
2025-10-01 17:23:32 +0800

    Create Day24.java

---
+++ Day24.java
@@ -0,0 +1,11 @@
+package YusnulMagfira;
+
+public class Day24 {
+    public static void main(String[] args) {
+        double jari = 5;
+        double luas = 3.14 * jari * jari;
+
+        System.out.println("jari-jari lingkaran : " + jari); //hasilnya 5
+        System.out.println("luas lingkaran : " + luas); //hasilnya 78.5
+    }
+}
