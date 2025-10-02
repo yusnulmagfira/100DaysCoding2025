7cabccebc5928ad41145e13ee713c6fac7b62ee5
yusnulmagfira <yusnulmagfira@gmail.com>
2025-10-02 19:19:42 +0800

    Day25.java

--- Day25.java
+++ Day25.java
@@ -3,13 +3,15 @@ package YusnulMagfira;
 public class Day25 {
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 5 ; i++) {
-            System.out.println("nilai i : " + i);
-        } for (int j = 5; j >= 1; j--) {
-                System.out.println("nilai j : " + j);
-            }
-
-
+            int a = 5;
+        int b = 5;
+//        Incremen
+        a++;
+//        Decrement
+        b--;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        
         }
     }
 
