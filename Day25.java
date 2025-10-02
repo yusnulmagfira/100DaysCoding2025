7135cd9906b076e4ae9dd184fbc746ac42727575
yusnulmagfira <yusnulmagfira@gmail.com>
2025-10-02 18:44:07 +0800

    Create Day25.java

---
+++ Day25.java
@@ -0,0 +1,15 @@
+package YusnulMagfira;
+
+public class Day25 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5 ; i++) {
+            System.out.println("nilai i : " + i);
+        } for (int j = 5; j >= 1; j--) {
+                System.out.println("nilai j : " + j);
+            }
+
+
+        }
+    }
+
