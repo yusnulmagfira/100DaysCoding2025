3b6e788387f22d534a34757b0134b0dac1948a4c
yusnulmagfira <yusnulmagfira@gmail.com>
2025-10-03 14:46:29 +0800

    Create Day26.java

---
+++ Day26.java
@@ -0,0 +1,24 @@
+package YusnulMagfira;
+
+public class Day26 {
+    public static void main(String[] args) {
+        //operator penugasan
+
+        int angka = 5;
+
+        angka += 2;
+        System.out.println("nilai angka : " + angka); //hasilnya 7
+
+        angka -=2;
+        System.out.println("nilai angka : " + angka); //hasilnya 5
+
+        angka *=2;
+        System.out.println("nilai angka : " + angka); //hasilnya 10
+
+        angka /=2;
+        System.out.println("nilai angka : " + angka); //hasilnya 5
+
+        angka %=2;
+        System.out.println("nilai angka : " + angka); //hasilnya 1
+    }
+}
