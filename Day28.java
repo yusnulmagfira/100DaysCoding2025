afda9c5ebbaba474e5370db03bd71960ecea5023
yusnulmagfira <yusnulmagfira@gmail.com>
2025-10-05 21:11:28 +0800

    Create Day28.java

---
+++ Day28.java
@@ -0,0 +1,11 @@
+package YusnulMagfira;
+
+public class Day28 {
+    public static void main(String[] args) {
+        int angka = 4;
+        int angka1 = 5;
+
+        System.out.println(angka == angka1);//hasilnya false
+        System.out.println(angka != angka1);//hasilnya true
+    }
+}
