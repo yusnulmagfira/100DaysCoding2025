37aba6066bacab4c3df36c957b68a662dea9a821
yusnulmagfira <yusnulmagfira@gmail.com>
2025-10-06 17:55:57 +0800

    Create Day29.java

---
+++ Day29.java
@@ -0,0 +1,14 @@
+package YusnulMagfira;
+
+public class Day29 {
+    public static void main(String[] args) {
+
+        //operator perbandingan
+
+        int angka = 5;
+        int angka1 = 7;
+
+        System.out.println(angka < angka1); //hasilnya true
+        System.out.println(angka > angka1); // hasilnya false
+    }
+}
