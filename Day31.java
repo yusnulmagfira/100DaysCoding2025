603885029b0aac7086c36a8987cff1318f39639f
yusnulmagfira <yusnulmagfira@gmail.com>
2025-10-08 20:01:50 +0800

    Create Day31.java

---
+++ Day31.java
@@ -0,0 +1,11 @@
+package YusnulMagfira;
+
+public class Day31 {
+    public static void main(String[] args) {
+        int angka = 2;
+        int angka1 = 8;
+
+        System.out.println(angka < angka1 && angka1 > angka); //hasilnya true
+
+    }
+}
