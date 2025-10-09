8c08d7f0fed7deb6bc0cc5404c8a2b9fbc1e223d
yusnulmagfira <yusnulmagfira@gmail.com>
2025-10-09 18:51:38 +0800

    Create Day32.java

---
+++ Day32.java
@@ -0,0 +1,15 @@
+package YusnulMagfira;
+
+public class Day32 {
+    public static void main(String[] args) {
+
+        int angka = 3;
+        int angka1 = 5;
+
+        if(angka > 2 || angka1 > 2) {
+            System.out.println("hasilnnya true");
+        }else{
+            System.out.println("hasilnya false");
+        }
+    }
+}
