ff4e62fa6e1b67beb651d8940e2a5ae29ff95a50
yusnulmagfira <yusnulmagfira@gmail.com>
2025-10-10 19:31:45 +0800

    Create Day33.java

---
+++ Day33.java
@@ -0,0 +1,12 @@
+package YusnulMagfira;
+
+public class Day33 {
+    public static void main(String[] args) {
+
+        int angka = 7;
+        int angka1 = 7;
+
+        boolean hasil = angka != angka1;
+        System.out.println(hasil);
+    }
+}
