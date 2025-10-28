19aa94d3bfd03f2137e970b813fd9f233e98c565
yusnulmagfira <yusnulmagfira@gmail.com>
2025-10-28 18:44:00 +0800

    Create Day51.java

---
+++ Day51.java
@@ -0,0 +1,10 @@
+package YusnulMagfira;
+
+public class Day51 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 7  ; i++) {
+            System.out.println("angka ke-" + i);
+
+        }
+    }
+}
