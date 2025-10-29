c697940cf4afa55bb4a2a480c69514d6e5741b33
yusnulmagfira <yusnulmagfira@gmail.com>
2025-10-29 18:56:48 +0800

    Create Day52.java

---
+++ Day52.java
@@ -0,0 +1,11 @@
+package YusnulMagfira;
+
+public class Day52 {
+    public static void main(String[] args) {
+    int angka = 1;
+    while (angka <= 5){
+        System.out.println("Angka : " +angka);
+        angka++;
+    }
+    }
+}
