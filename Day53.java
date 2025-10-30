adb3b3008a13bcf8086b2ac2378e34bb0a254307
yusnulmagfira <yusnulmagfira@gmail.com>
2025-10-30 18:33:47 +0800

    Create Day53.java

---
+++ Day53.java
@@ -0,0 +1,16 @@
+package YusnulMagfira;
+
+public class Day53 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i == 5) {
+                break;
+            }
+            System.out.println("Angka: " + i);
+        }
+
+    }
+}
+
+
+
