e50ed4d4e23992cb0bcd88a1ae7d40d823c13c1e
yusnulmagfira <yusnulmagfira@gmail.com>
2025-10-31 20:11:19 +0800

    Create Day54.java

---
+++ Day54.java
@@ -0,0 +1,12 @@
+package YusnulMagfira;
+
+public class Day54 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            if (i == 3) {
+                continue;
+            }
+            System.out.println("Angka: " + i);
+        }
+    }
+}
