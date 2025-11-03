13779bf697abccda30ecedece1bfea4734393603
yusnulmagfira <yusnulmagfira@gmail.com>
2025-11-03 19:16:54 +0800

    Create Day57.java

---
+++ Day57.java
@@ -0,0 +1,13 @@
+package YusnulMagfira;
+
+public class Day57 {
+    public static void main(String[] args) {
+        for (int i = 1; i <=2 ; i++) {
+            System.out.println("perulangan keluar: " + i);
+
+        for (int j = 1; j <=3 ; j++) {
+            System.out.println("perulangan kedalam: " + j);
+        }
+        }
+    }
+}
