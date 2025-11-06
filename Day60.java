71bfb81d7b7b5cab12009cb75e973f090aac7ccd
yusnulmagfira <yusnulmagfira@gmail.com>
2025-11-06 16:45:42 +0800

    Create Day60.java

---
+++ Day60.java
@@ -0,0 +1,26 @@
+package YusnulMagfira;
+
+import java.util.Scanner;
+
+public class Day60 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan angka N : ");
+        int angka = sc.nextInt();
+            for (int i = angka; i >= 1 ; i--) {
+                if(i % 2 == 1){
+                    System.out.println("Menampilkan angka ganjil : " + i);
+                }
+            }
+            System.out.println("--------------------------------------");
+            for (int i = angka; i >= 1 ; i--) {
+                if(i % 2 == 0) {
+                    System.out.println("Menampilkan angka genap : " + i);
+                }
+            }
+
+        }
+    }
+
+
