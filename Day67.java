86ed9985e6aecc8b081867532a86f102aa2c451b
yusnulmagfira <yusnulmagfira@gmail.com>
2025-11-13 20:42:42 +0800

    Create Day67.java

---
+++ Day67.java
@@ -0,0 +1,10 @@
+package YusnulMagfira;
+
+public class Day67 {
+    public static void main(String[] args) {
+        int angka = 5;
+        for (int i = 1; i <= angka ; i++) {
+            System.out.print(" * ");
+        }
+    }
+}
