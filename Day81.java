dd0023bfdef2d882bfbf3cf5b1094eb3a1978524
yusnulmagfira <yusnulmagfira@gmail.com>
2025-11-27 23:10:11 +0800

    Create Day81.java

---
+++ Day81.java
@@ -0,0 +1,8 @@
+package YusnulMagfira;
+
+public class Day81 {
+    public static void main(String[] args) {
+        int [] angka = {1, 4, 6, 9, 11};
+         System.out.println("tampilkan indeks ke-: "+angka[3]);
+        }
+    }
