c26332b296c4288d14c528e2b779f334dab5b2b7
yusnulmagfira <yusnulmagfira@gmail.com>
2025-11-28 19:51:23 +0800

    Create Day82.java

---
+++ Day82.java
@@ -0,0 +1,12 @@
+package YusnulMagfira;
+
+public class Day82 {
+    public static void main(String[] args) {
+        int[] angka = {1, 4, 6, 8, 9, 5};
+        for (int i = 0; i <= angka.length ; i++) {
+
+            System.out.println(angka[i]);
+        }
+
+    }
+}
