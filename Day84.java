781555a932a228467ba0bbfe75138d06b47c64db
yusnulmagfira <yusnulmagfira@gmail.com>
2025-11-30 20:37:30 +0800

    Create Day84.java

---
+++ Day84.java
@@ -0,0 +1,15 @@
+package YusnulMagfira;
+
+import java.util.Scanner;
+public class Day84 {
+    public static void main(String[] args) {
+
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Masukkan Nilai : ");
+        int angka = sc.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i + " x " + angka + " = " + (i * angka));
+        }
+    }
+}
