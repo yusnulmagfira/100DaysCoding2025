235b3053a931c60478afe535f78869bfc5bc3326
yusnulmagfira <yusnulmagfira@gmail.com>
2025-12-04 22:57:06 +0800

    Create Day88.java

---
+++ Day88.java
@@ -0,0 +1,11 @@
+package YusnulMagfira;
+
+public class Day88 {
+    public static void main(String[] args) {
+        int[] nilai = {90, 91, 98, 99};
+
+        for (int angka : nilai) {
+            System.out.println("Nilai: " + angka);
+        }
+    }
+}
