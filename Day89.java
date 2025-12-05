1cbee71aeaa3dd1c7c6b73f79bd0cf3741bcaf0a
yusnulmagfira <yusnulmagfira@gmail.com>
2025-12-05 22:46:11 +0800

    Create Day89.java

---
+++ Day89.java
@@ -0,0 +1,11 @@
+package YusnulMagfira;
+
+public class Day89 {
+    static void segitiga (int alas, int tinggi){
+        int total1 = alas * tinggi;
+        System.out.println(total1);
+    }
+    public static void main(String[] args) {
+        segitiga(5, 6);
+    }
+}
