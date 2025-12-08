fed5ea3e5e5071eda813a9bd816d0bf3800e699f
yusnulmagfira <yusnulmagfira@gmail.com>
2025-12-08 22:02:09 +0800

    Create Day92.java

---
+++ Day92.java
@@ -0,0 +1,11 @@
+package YusnulMagfira;
+
+public class Day92 {
+    static int tambah(int angka1, int angka2) {
+        return angka1 + angka2;
+    }
+    public static void main(String[] args) {
+        int hasil1 = tambah(5, 10);
+        System.out.println("Hasil 5 + 10 = " + hasil1);
+    }
+}
