bc55e65ecfaccc296bf07c639051581f6a6d12fe
yusnulmagfira <yusnulmagfira@gmail.com>
2025-12-09 23:02:23 +0800

    Create Day93.java

---
+++ Day93.java
@@ -0,0 +1,10 @@
+public class Day93 {
+    public static void main(String[] args) {
+        double angka = 2.3;
+
+        System.out.println("Angka asli: " + angka);
+        System.out.println("Math.ceil() → " + Math.ceil(angka));
+        System.out.println("Math.floor() → " + Math.floor(angka));
+        System.out.println("Math.round() → " + Math.round(angka));
+    }
+}
