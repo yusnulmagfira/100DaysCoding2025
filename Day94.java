ba02a24a695a7c34f6d5f4404d50b11c73397816
yusnulmagfira <yusnulmagfira@gmail.com>
2025-12-10 20:35:57 +0800

    Create Day94.java

---
+++ Day94.java
@@ -0,0 +1,10 @@
+package YusnulMagfira;
+
+public class Day94 {
+    public static void panggil(){
+        System.out.println("Pilong");
+    }
+    public static void main(String[] args) {
+        panggil();
+    }
+}
