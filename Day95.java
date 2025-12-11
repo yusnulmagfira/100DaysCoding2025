ecaeebe3f01b35d28d270c6d50a4598be344bcd3
yusnulmagfira <yusnulmagfira@gmail.com>
2025-12-11 22:53:54 +0800

    Create Day95.java

---
+++ Day95.java
@@ -0,0 +1,11 @@
+package YusnulMagfira;
+
+public class Day95 {
+    public static void cetak(String pesan){
+        System.out.println(pesan);
+
+        }
+    public static void main(String[] args) {
+        cetak("anynyeong yorobun");
+    }
+}
